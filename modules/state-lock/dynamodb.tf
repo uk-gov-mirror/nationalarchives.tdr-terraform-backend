07a8535765b162b5c9f1ff45ddcfa67906b935e2
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-11-21 14:29:05 +0000

    Changes to get Terraform working across accounts
    Added permission to access DynamoDb table controlling the state lock
    
    Added Terraform backend for Jenkins in the management account

--- modules/state-lock/dynamodb.tf
+++ modules/state-lock/dynamodb.tf
@@ -12,7 +12,26 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   tags = merge(
     var.common_tags,
     map(
-      "Name", "TDR Terraform State Lock Table ",
+      "Name", "TDR Terraform State Lock Table",
+    )
+  )
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock_jenkins" {
+  name           = "tdr-terraform-state-lock-jenkins"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = merge(
+    var.common_tags,
+    map(
+      "Name", "TDR Jenkins Terraform State Lock Table",
     )
   )
 }
