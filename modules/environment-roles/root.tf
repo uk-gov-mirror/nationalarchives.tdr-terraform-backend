89406c8c2ab52d9e4e455cb15640d5f0195db71a
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-12-06 14:25:25 +0000

    Renamed TDR environments
    TDR environments renamed to intg, staging, prod to match accounts
    
    Updated README with details on running project

--- modules/environment-roles/root.tf
+++ modules/environment-roles/root.tf
@@ -1,5 +1,7 @@
 locals {
-  environment = lookup(var.workspace_to_environment_map, terraform.workspace, "ci")
+  environment         = lookup(var.workspace_to_environment_map, terraform.workspace, "intg")
+  //environment_account = lookup(var.workspace_environment_account_map, terraform.workspace, "ci" )
+  environment_profile = lookup(var.workspace_aws_profile_map, terraform.workspace, "intg")
   common_tags = map(
     "Owner", "TDR",
     "Terraform", true
@@ -8,6 +10,7 @@ locals {
 
 provider "aws" {
   region  = "eu-west-2"
+  profile = local.environment_profile
 }
 
 data "template_file" "terraform_assume_role_policy" {
@@ -39,7 +42,7 @@ data "template_file" "s3_terraform_policy" {
 }
 
 resource "aws_iam_policy" "s3_terraform" {
-  name        = "s3-terraform-policy"
+  name        = "s3-terraform-policy-${local.environment}"
   description = "Policy to give permission to Terraform s3 buckets"
   policy      = data.template_file.s3_terraform_policy.rendered
 }
--- modules/environment-roles/variables.tf
+++ modules/environment-roles/variables.tf
@@ -3,8 +3,29 @@ variable "workspace_to_environment_map" {
 
   //Maps the Terraform workspace to the AWS environment.
   default = {
-    ci   = "ci"
-    test = "test"
+    intg   = "intg"
+    staging = "staging"
     prod = "prod"
+  }
+}
+
+//variable "workspace_environment_account_map" {
+//  type = map(string)
+//
+//  //Maps the Terraform workspace to the AWS environment account number.
+//  default = {
+//    ci   = "229554778675"
+//    test = "846822818902"
+//    prod = "846822818902"
+//  }
+//}
+
+variable "workspace_aws_profile_map" {
+  type = map(string)
+
+  default = {
+    intg   = "intgaccess"
+    staging = "prodaccess"
+    prod = "prodaccess"
   }
 }
