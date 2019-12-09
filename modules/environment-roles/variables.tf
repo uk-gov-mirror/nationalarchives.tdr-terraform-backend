cbccc3cbfbf013da45f2dae6779b91f6606b871b
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-12-09 09:33:13 +0000

    Updated setup instuctions
    Added a "bootstrap" s3 backend to AWS manually to allow all developers to update the backend

--- modules/environment-roles/variables.tf
+++ modules/environment-roles/variables.tf
@@ -3,29 +3,18 @@ variable "workspace_to_environment_map" {
 
   //Maps the Terraform workspace to the AWS environment.
   default = {
-    intg   = "intg"
+    intg    = "intg"
     staging = "staging"
-    prod = "prod"
+    prod    = "prod"
   }
 }
-
-//variable "workspace_environment_account_map" {
-//  type = map(string)
-//
-//  //Maps the Terraform workspace to the AWS environment account number.
-//  default = {
-//    ci   = "229554778675"
-//    test = "846822818902"
-//    prod = "846822818902"
-//  }
-//}
 
 variable "workspace_aws_profile_map" {
   type = map(string)
 
   default = {
-    intg   = "intgaccess"
+    intg    = "intgaccess"
     staging = "prodaccess"
-    prod = "prodaccess"
+    prod    = "prodaccess"
   }
 }
