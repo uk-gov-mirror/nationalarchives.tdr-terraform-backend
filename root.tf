c0b0fac356cf1c28999bef11826401a0937b34eb
Sam Palmer <github@sampalmer.dev>
2019-12-17 15:41:38 +0000

    Ran terraform fmt

--- root.tf
+++ root.tf
@@ -33,15 +33,15 @@ module "terraform_state_lock" {
 }
 
 module "terraform_permissions" {
-  source = "./modules/permissions"
+  source                 = "./modules/permissions"
   common_tags            = local.common_tags
   terraform_state_bucket = module.terraform_state.terraform_state_bucket_arn
   terraform_state_lock   = module.terraform_state_lock.terraform_state_lock_arn
 }
 
 module "jenkins_permissions" {
-  source = "./modules/jenkins"
-  environment = "mgmt"
+  source                         = "./modules/jenkins"
+  environment                    = "mgmt"
   terraform_jenkins_state_bucket = module.terraform_state.terraform_jenkins_state_bucket
-  terraform_jenkins_state_lock = module.terraform_state_lock.terraform_jenkins_state_lock
+  terraform_jenkins_state_lock   = module.terraform_state_lock.terraform_jenkins_state_lock
 }
