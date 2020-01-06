f9a5eaeba3fb96a079f6a5ba82a48e9aa15d01ff
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-06 10:16:53 +0000

    Refactor environment roles terraform
    environment-roles module refactored to remove the need to run this separately for each TDR environment
    
    environment-roles module supplied with different AWS provider from the root.tf to allow assumption of the correct IAM role. This removes the need for setting up different AWS profiles locally.
    
    Backend Terraform can be run using a single set of credentials from the management account.
    
    Refactored the permissions module to split out the specific environment roles into a different modules to remove multiple nested modules within Terraform.

--- root.tf
+++ root.tf
@@ -5,6 +5,18 @@ locals {
   )
 }
 
+data "aws_ssm_parameter" "intg_account_number" {
+  name = "/mgmt/intg_account"
+}
+
+data "aws_ssm_parameter" "staging_account_number" {
+  name = "/mgmt/staging_account"
+}
+
+data "aws_ssm_parameter" "prod_account_number" {
+  name = "/mgmt/prod_account"
+}
+
 terraform {
   backend "s3" {
     bucket         = "tdr-bootstrap-terraform-state"
@@ -15,23 +27,89 @@ terraform {
   }
 }
 
+//Management account AWS provider
 provider "aws" {
   region  = "eu-west-2"
   profile = "management"
 }
 
+//AWS providers for TDR environment accounts
+provider "aws" {
+  alias   = "intg"
+  region  = "eu-west-2"
+  profile = "management"
+  assume_role {
+    role_arn     = "arn:aws:iam::${data.aws_ssm_parameter.intg_account_number.value}:role/IAM_Admin_Role"
+    session_name = "terraform-backend"
+  }
+}
+
+provider "aws" {
+  alias   = "staging"
+  region  = "eu-west-2"
+  profile = "management"
+  assume_role {
+    role_arn     = "arn:aws:iam::${data.aws_ssm_parameter.staging_account_number.value}:role/IAM_Admin_Role"
+    session_name = "terraform-backend"
+  }
+}
+
+provider "aws" {
+  alias   = "prod"
+  region  = "eu-west-2"
+  profile = "management"
+  assume_role {
+    role_arn     = "arn:aws:iam::${data.aws_ssm_parameter.prod_account_number.value}:role/IAM_Admin_Role"
+    session_name = "terraform-backend"
+  }
+}
+
+//Set up TDR environment roles to provide permissions for Terraform
+module "intg_environment_roles" {
+  source = "./modules/environment-roles"
+  providers = {
+    aws = aws.intg
+  }
+
+  tdr_environment = "intg"
+  common_tags     = local.common_tags
+}
+
+module "staging_environment_role" {
+  source = "./modules/environment-roles"
+  providers = {
+    aws = aws.staging
+  }
+
+  tdr_environment = "staging"
+  common_tags     = local.common_tags
+}
+
+module "prod_environment_role" {
+  source = "./modules/environment-roles"
+  providers = {
+    aws = aws.prod
+  }
+
+  tdr_environment = "prod"
+  common_tags     = local.common_tags
+}
+
+//Set up Terraform Backend state
 module "terraform_state" {
   source = "./modules/state"
 
   common_tags = local.common_tags
 }
 
+//Set up Terraform Backend statelock
 module "terraform_state_lock" {
   source = "./modules/state-lock"
 
   common_tags = local.common_tags
 }
 
+//Set up common IAM policies for Terraform
 module "terraform_permissions" {
   source                 = "./modules/permissions"
   common_tags            = local.common_tags
@@ -39,6 +117,44 @@ module "terraform_permissions" {
   terraform_state_lock   = module.terraform_state_lock.terraform_state_lock_arn
 }
 
+//Set up specific TDR environment IAM policies for Terraform
+module "intg_specific_permissions" {
+  source                          = "./modules/specific-environment-permissions"
+  common_tags                     = local.common_tags
+  terraform_state_bucket          = module.terraform_state.terraform_state_bucket_arn
+  terraform_state_lock            = module.terraform_state_lock.terraform_state_lock_arn
+  tdr_account_number              = data.aws_ssm_parameter.intg_account_number.value
+  tdr_environment                 = "intg"
+  read_terraform_state_policy_arn = module.terraform_permissions.read_terraform_state_policy_arn
+  terraform_state_lock_access_arn = module.terraform_permissions.terraform_state_lock_access_arn
+  terraform_describe_account_arn  = module.terraform_permissions.terraform_describe_account_arn
+}
+
+module "staging_specific_permissions" {
+  source                          = "./modules/specific-environment-permissions"
+  common_tags                     = local.common_tags
+  terraform_state_bucket          = module.terraform_state.terraform_state_bucket_arn
+  terraform_state_lock            = module.terraform_state_lock.terraform_state_lock_arn
+  tdr_account_number              = data.aws_ssm_parameter.staging_account_number.value
+  tdr_environment                 = "staging"
+  read_terraform_state_policy_arn = module.terraform_permissions.read_terraform_state_policy_arn
+  terraform_state_lock_access_arn = module.terraform_permissions.terraform_state_lock_access_arn
+  terraform_describe_account_arn  = module.terraform_permissions.terraform_describe_account_arn
+}
+
+module "prod_specific_permissions" {
+  source                          = "./modules/specific-environment-permissions"
+  common_tags                     = local.common_tags
+  terraform_state_bucket          = module.terraform_state.terraform_state_bucket_arn
+  terraform_state_lock            = module.terraform_state_lock.terraform_state_lock_arn
+  tdr_account_number              = data.aws_ssm_parameter.prod_account_number.value
+  tdr_environment                 = "prod"
+  read_terraform_state_policy_arn = module.terraform_permissions.read_terraform_state_policy_arn
+  terraform_state_lock_access_arn = module.terraform_permissions.terraform_state_lock_access_arn
+  terraform_describe_account_arn  = module.terraform_permissions.terraform_describe_account_arn
+}
+
+//Set up Jenkins permissions
 module "jenkins_permissions" {
   source                         = "./modules/jenkins"
   environment                    = "mgmt"
