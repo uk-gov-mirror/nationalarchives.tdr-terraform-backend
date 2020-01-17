ff9ed14ad56bec0782bbf3ea4a51b134c9314255
Sam Palmer <github@sampalmer.dev>
2020-01-17 15:33:52 +0000

    Refactor terraform policies.
    There were many statements in the policy that were applied globally or
    for none service specific resources and these statements were being
    applied for each service, which is pointless.
    
    This change splits out the common statements which then allows the
    service specific statements to fit into a single policy.

--- modules/state/s3.tf
+++ modules/state/s3.tf
@@ -39,3 +39,16 @@ resource "aws_s3_bucket_public_access_block" "public_access_block_jenkins" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+resource "aws_s3_bucket" "tdr_database_migrations" {
+  bucket = "tdr-database-migrations"
+  acl    = "private"
+  policy = file("${path.module}/policy.json.tpl")
+  tags = merge(
+  var.common_tags,
+  map(
+  "Name", "TDR Jekins Terraform State",
+  )
+  )
+}
