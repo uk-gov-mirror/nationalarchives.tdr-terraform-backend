89347719a38bcd4ac92759bd40f71e899d58db94
Sam Palmer <github@sampalmer.dev>
2020-01-22 08:09:11 +0000

    Remove tdr-migrations bucket and permissions.
    The sql files for the migrations are now going to live in the same
    project as the flyway lambda so there's no need for copying them to and
    from s3.

--- modules/state/s3.tf
+++ modules/state/s3.tf
@@ -40,15 +40,3 @@ resource "aws_s3_bucket_public_access_block" "public_access_block_jenkins" {
   restrict_public_buckets = true
 }
 
-
-resource "aws_s3_bucket" "tdr_database_migrations" {
-  bucket = "tdr-database-migrations"
-  acl    = "private"
-  policy = file("${path.module}/policy.json.tpl")
-  tags = merge(
-    var.common_tags,
-    map(
-      "Name", "TDR Jekins Terraform State",
-    )
-  )
-}
