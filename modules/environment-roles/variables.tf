ccb6f332cf66234548bcded4de5da55d96337c06
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-28 10:53:57 +0000

    Add environment role to read params
    End to end test require keycloak permissions to create temporary test user credentials to run automated test
    
    Role in each environment to allow reading of SSM parameters to retrieve keycloak admin user to create temporary test users

--- modules/environment-roles/variables.tf
+++ modules/environment-roles/variables.tf
@@ -7,3 +7,8 @@ variable "common_tags" {
   description = "Common tags to be applied to all resources"
   type        = map(string)
 }
+
+variable "tdr_mgmt_account_number" {
+  description = "AWS management account number"
+  type = string
+}
