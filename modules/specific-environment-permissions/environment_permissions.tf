10e4871a6bc2abd0c3d13b7028b734f71f57ec23
TomJKing <Tom.King@nationalarchives.gov.uk>
2020-01-30 07:03:59 +0000

    Add assume role permissions to master account

--- modules/specific-environment-permissions/environment_permissions.tf
+++ modules/specific-environment-permissions/environment_permissions.tf
@@ -132,7 +132,7 @@ resource "aws_iam_policy" "jenkins_read_params_policy" {
 
 resource "aws_iam_role_policy_attachment" "jenkins_role_read_params_attachment" {
   policy_arn = aws_iam_policy.jenkins_read_params_policy.arn
-  role       = aws_iam_role.jenkins_read_params_assume_role
+  role       = aws_iam_role.jenkins_read_params_assume_role.name
 }
 
 data "aws_iam_policy_document" "jenkins_node_read_params_document" {
