fff54c5632d0c20e43d0dc105ddf5d4aca197d4d
TomJKing <tom.king@nationalarchives.gov.uk>
2020-09-14 12:30:42 +0100

    Add Grafana Terraform state
    S3 bucket and DynamoDb resources to store Terraform state for Grafana
    
    Single Grafana instances running on TDR management account

--- modules/state-lock/output.tf
+++ modules/state-lock/output.tf
@@ -5,3 +5,7 @@ output "terraform_state_lock_arn" {
 output "terraform_jenkins_state_lock" {
   value = aws_dynamodb_table.terraform_state_lock_jenkins.arn
 }
+
+output "terraform_grafana_state_lock_arn" {
+  value = aws_dynamodb_table.terraform_state_lock_grafana.arn
+}
