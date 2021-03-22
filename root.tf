751640f6d0baa9e3d701368b2f051cbacac3587b
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2021-03-22 12:13:08 +0000

    Pass list of muted ECR alerts to notifications lambda
    These are CVEs for which no fix is available, and which should be
    temporarily muted in the email and Slack alerts.

--- root.tf
+++ root.tf
@@ -26,6 +26,10 @@ data "aws_ssm_parameter" "cost_centre" {
   name = "/mgmt/cost_centre"
 }
 
+module "global_parameters" {
+  source = "./tdr-configurations/terraform"
+}
+
 terraform {
   backend "s3" {
     bucket         = "tdr-bootstrap-terraform-state"
@@ -333,6 +337,7 @@ module "notification_lambda" {
   lambda_ecr_scan_notifications = true
   event_rule_arns               = [module.ecr_image_scan_event.event_arn, "arn:aws:events:eu-west-2:${data.aws_ssm_parameter.mgmt_account_number.value}:rule/jenkins-backup-maintenance-window"]
   sns_topic_arns                = ["arn:aws:sns:eu-west-2:${data.aws_ssm_parameter.intg_account_number.value}:tdr-notifications-intg", "arn:aws:sns:eu-west-2:${data.aws_ssm_parameter.staging_account_number.value}:tdr-notifications-staging", "arn:aws:sns:eu-west-2:${data.aws_ssm_parameter.prod_account_number.value}:tdr-notifications-prod"]
+  muted_scan_alerts             = module.global_parameters.muted_ecr_scan_alerts
 }
 
 module "periodic_ecr_image_scan_lambda" {
