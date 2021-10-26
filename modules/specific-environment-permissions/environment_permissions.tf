e3c01b8d6cb10ca5cbef8502bcbdd4580a0b7003
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-10-26 13:10:00 +0100

    Add permissions to run performance checks.
    The Jenkins assume role for the sandbox environment is used to run the
    performance checks. These checks need to get parameters in the sandbox
    environment and download dependencies for our local libraries from sbt.
    
    Also, the role itself needs to be created.

--- modules/specific-environment-permissions/environment_permissions.tf
+++ modules/specific-environment-permissions/environment_permissions.tf
@@ -219,6 +219,18 @@ resource "aws_iam_role_policy_attachment" "terraform_role_access_terraform_state
   policy_arn = var.terraform_state_lock_access_arn
 }
 
+resource "aws_iam_role_policy_attachment" "jenkins_publish_policy_attach" {
+  count      = var.jenkins_publish_policy_arn == "" ? 0 : 1
+  policy_arn = var.jenkins_publish_policy_arn
+  role       = aws_iam_role.terraform_assume_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_read_only_attach" {
+  count      = var.add_ssm_policy == true ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+  role       = aws_iam_role.terraform_assume_role.name
+}
+
 resource "aws_iam_role_policy_attachment" "terraform_role_describe_accounts" {
   role       = aws_iam_role.terraform_assume_role.name
   policy_arn = var.terraform_describe_account_arn
