ab3a7e9d7a658f077db480d6514b61b40ee6e69f
TomJKing <tom.king@nationalarchives.gov.uk>
2022-01-26 09:04:27 +0000

    Tidy up providers

--- versions.tf
+++ versions.tf
@@ -7,13 +7,3 @@ terraform {
   }
   required_version = ">= 1.1.3"
 }
-
-provider "aws" {
-  alias   = "intg"
-  region  = "eu-west-2"
-  profile = "management"
-  assume_role {
-    role_arn     = "arn:aws:iam::${data.aws_ssm_parameter.intg_account_number.value}:role/IAM_Admin_Role"
-    session_name = "terraform-backend"
-  }
-}
