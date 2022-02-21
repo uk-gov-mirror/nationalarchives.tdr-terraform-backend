c907367682276cfb7e4346349e204b8825685564
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-02-21 17:07:29 +0000

    Run terraform init

--- root.tf
+++ root.tf
@@ -372,50 +372,50 @@ module "periodic_ecr_image_scan_event" {
 }
 
 module "run_e2e_tests_role" {
-  source = "./tdr-terraform-modules/iam_role"
-  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {account_id = data.aws_ssm_parameter.mgmt_account_number.value, repo_name = "tdr-e2e-tests:*"})
-  common_tags = local.common_tags
-  name = "GithubActionsE2ETestsMgmt"
+  source             = "./tdr-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", { account_id = data.aws_ssm_parameter.mgmt_account_number.value, repo_name = "tdr-e2e-tests:*" })
+  common_tags        = local.common_tags
+  name               = "GithubActionsE2ETestsMgmt"
   policy_attachments = {
-    export_intg = "arn:aws:iam::${data.aws_ssm_parameter.mgmt_account_number.value}:policy/TDRJenkinsNodeS3ExportPolicyIntg"
+    export_intg    = "arn:aws:iam::${data.aws_ssm_parameter.mgmt_account_number.value}:policy/TDRJenkinsNodeS3ExportPolicyIntg"
     export_staging = "arn:aws:iam::${data.aws_ssm_parameter.mgmt_account_number.value}:policy/TDRJenkinsNodeS3ExportPolicyStaging"
   }
 }
 
 module "github_sbt_dependencies_policy" {
-  source = "./tdr-terraform-modules/iam_policy"
-  name   = "TDRGithubDependenciesPolicyMgmt"
+  source        = "./tdr-terraform-modules/iam_policy"
+  name          = "TDRGithubDependenciesPolicyMgmt"
   policy_string = templatefile("${path.module}/templates/iam_policy/github_sbt_dependencies_policy.json.tpl", {})
 }
 
 module "github_ecr_policy" {
-  source = "./tdr-terraform-modules/iam_policy"
-  name   = "TDRGithubECRPolicyMgmt"
-  policy_string = templatefile("${path.module}/templates/iam_policy/github_ecr_policy.json.tpl", {account_id = data.aws_ssm_parameter.mgmt_account_number.value})
+  source        = "./tdr-terraform-modules/iam_policy"
+  name          = "TDRGithubECRPolicyMgmt"
+  policy_string = templatefile("${path.module}/templates/iam_policy/github_ecr_policy.json.tpl", { account_id = data.aws_ssm_parameter.mgmt_account_number.value })
 }
 
 module "github_actions_code_bucket_policy" {
-  source = "./tdr-terraform-modules/iam_policy"
-  name   = "TDRGithubActionsBackendCodeMgmt"
+  source        = "./tdr-terraform-modules/iam_policy"
+  name          = "TDRGithubActionsBackendCodeMgmt"
   policy_string = templatefile("${path.module}/templates/iam_policy/github_code_bucket.json.tpl", {})
 }
 
 module "github_actions_role" {
-  source = "./tdr-terraform-modules/iam_role"
-  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", {account_id = data.aws_ssm_parameter.mgmt_account_number.value, repo_name = "tdr-*"})
-  common_tags = local.common_tags
-  name = "TDRGithubActionsRoleMgmt"
+  source             = "./tdr-terraform-modules/iam_role"
+  assume_role_policy = templatefile("${path.module}/templates/iam_role/github_assume_role.json.tpl", { account_id = data.aws_ssm_parameter.mgmt_account_number.value, repo_name = "tdr-*" })
+  common_tags        = local.common_tags
+  name               = "TDRGithubActionsRoleMgmt"
   policy_attachments = {
     dependencies = module.github_sbt_dependencies_policy.policy_arn,
-    ecr = module.github_ecr_policy.policy_arn
+    ecr          = module.github_ecr_policy.policy_arn
     backend_code = module.github_actions_code_bucket_policy.policy_arn
   }
 }
 
 module "github_oidc_provider" {
-  source = "./tdr-terraform-modules/identity_provider"
-  audience = "sts.amazonaws.com"
-  thumbprint = "6938fd4d98bab03faadb97b34396831e3780aea1"
-  url = "https://token.actions.githubusercontent.com"
+  source      = "./tdr-terraform-modules/identity_provider"
+  audience    = "sts.amazonaws.com"
+  thumbprint  = "6938fd4d98bab03faadb97b34396831e3780aea1"
+  url         = "https://token.actions.githubusercontent.com"
   common_tags = local.common_tags
 }
