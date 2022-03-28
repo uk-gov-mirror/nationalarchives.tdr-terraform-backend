fd082a58aa73bb3589b2dd53d158461ed72cac35
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-28 17:08:18 +0100

    Add secrets for the graphql client deploy job

--- root_github.tf
+++ root_github.tf
@@ -123,3 +123,16 @@ module "github_generated_graphql_environment" {
     SONATYPE_PASSWORD = data.aws_ssm_parameter.sonatype_password.value
   }
 }
+
+module "github_graphql_client_environment" {
+  source          = "./tdr-terraform-modules/github_repositories"
+  repository_name = "nationalarchives/tdr-graphql-client"
+  secrets = {
+    WORKFLOW_PAT      = data.aws_ssm_parameter.workflow_pat.value
+    SLACK_WEBHOOK     = data.aws_ssm_parameter.slack_webhook_url.value
+    GPG_PASSPHRASE    = data.aws_ssm_parameter.gpg_passphrase.value
+    GPG_PRIVATE_KEY   = data.aws_ssm_parameter.gpg_key.value
+    SONATYPE_USERNAME = data.aws_ssm_parameter.sonatype_username.value
+    SONATYPE_PASSWORD = data.aws_ssm_parameter.sonatype_password.value
+  }
+}
