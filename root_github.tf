25d4f3df0573c10db9512376b83d7ec6e290babe
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-04-27 10:23:07 +0100

    Add secrets for API update jobs

--- root_github.tf
+++ root_github.tf
@@ -354,3 +354,13 @@ module "github_scripts_repository" {
     SLACK_WEBHOOK      = data.aws_ssm_parameter.slack_webhook_url.value
   }
 }
+
+module "github_api_update_repository" {
+  source          = "./tdr-terraform-modules/github_repositories"
+  repository_name = "nationalarchives/tdr-api-update"
+  secrets = {
+    MANAGEMENT_ACCOUNT = data.aws_ssm_parameter.mgmt_account_number.value
+    WORKFLOW_PAT       = data.aws_ssm_parameter.workflow_pat.value
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.slack_webhook_url.value
+  }
+}
