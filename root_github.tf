c41e731e2da2de3b3fb683749013f19d21b5744e
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-06 12:58:55 +0100

    Add permissions for the documentation repos.
    They only need the slack webhook to alert if secrets are found

--- root_github.tf
+++ root_github.tf
@@ -409,3 +409,19 @@ module "github_tna_custodian_repository" {
     SANDBOX_ACCOUNT_NUMBER = data.aws_ssm_parameter.sandbox_account_number.value
   }
 }
+
+module "github_dev_documentation_repository" {
+  source          = "./tdr-terraform-modules/github_repositories"
+  repository_name = "nationalarchives/tdr-dev-documentation"
+  secrets = {
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.slack_webhook_url.value
+  }
+}
+
+module "github_dev_documentation_internal_repository" {
+  source          = "./tdr-terraform-modules/github_repositories"
+  repository_name = "nationalarchives/tdr-dev-documentation-internal"
+  secrets = {
+    SLACK_WEBHOOK      = data.aws_ssm_parameter.slack_webhook_url.value
+  }
+}
