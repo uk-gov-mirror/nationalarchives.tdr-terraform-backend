b581f29c3a5e7f1a5111b787d109a54320417dde
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-02-03 14:14:57 +0000

    Remove old Jenkins roles.
    A lot of these have been flagged by the pen testers so it's time for a
    tidy up.

--- modules/environment-roles/root.tf
+++ modules/environment-roles/root.tf
@@ -107,20 +107,6 @@ data "aws_iam_policy_document" "frontend_storage_override" {
   }
 }
 
-resource "aws_iam_role" "tdr_jenkins_ecs_update_role" {
-  name               = "TDRJenkinsECSUpdateRole${title(var.tdr_environment)}"
-  assume_role_policy = templatefile("./modules/environment-roles/templates/terraform_assume_role_policy.json.tpl", { account_id = var.tdr_mgmt_account_number })
-}
-
-resource "aws_iam_role_policy_attachment" "tdr_jenkins_ecs_update_role_attach" {
-  policy_arn = aws_iam_policy.tdr_jenkins_update_ecs_policy.arn
-  role       = aws_iam_role.tdr_jenkins_ecs_update_role.name
-}
-
-resource "aws_iam_policy" "tdr_jenkins_update_ecs_policy" {
-  name   = "TDRJenkinsUpdateECS${title(var.tdr_environment)}"
-  policy = data.aws_iam_policy_document.tdr_jenkins_update_ecs_service.json
-}
 
 data "aws_iam_policy_document" "tdr_jenkins_update_ecs_service" {
   statement {
@@ -133,43 +119,6 @@ data "aws_iam_policy_document" "tdr_jenkins_update_ecs_service" {
       "arn:aws:ecs:eu-west-2:${data.aws_caller_identity.current.account_id}:service/consignmentapi_${var.tdr_environment}/consignmentapi_service_${var.tdr_environment}"
     ]
   }
-}
-
-resource "aws_iam_role" "tdr_jenkins_lambda_role" {
-  name               = "TDRJenkinsLambdaRole${title(var.tdr_environment)}"
-  assume_role_policy = templatefile("./modules/environment-roles/templates/terraform_assume_role_policy.json.tpl", { account_id = var.tdr_mgmt_account_number })
-}
-
-resource "aws_iam_role_policy_attachment" "tdr_jenkins_lambda_role_attach" {
-  policy_arn = aws_iam_policy.tdr_jenkins_lambda_policy.arn
-  role       = aws_iam_role.tdr_jenkins_lambda_role.name
-}
-
-resource "aws_iam_policy" "tdr_jenkins_lambda_policy" {
-  name   = "TDRJenkinsLambda${title(var.tdr_environment)}"
-  policy = templatefile("${path.module}/templates/jenkins_lambda_policy.json.tpl", { account_id = data.aws_caller_identity.current.account_id, title_environment = title(var.tdr_environment), environment = var.tdr_environment })
-}
-
-resource "aws_iam_role" "tdr_jenkins_read_params_role" {
-  name               = "TDRJenkinsReadParamsRole${title(var.tdr_environment)}"
-  assume_role_policy = templatefile("./modules/environment-roles/templates/terraform_assume_role_policy.json.tpl", { account_id = var.tdr_mgmt_account_number })
-
-  tags = merge(
-    var.common_tags,
-    tomap(
-      { "Name" = "${title(var.tdr_environment)} Read Parameters role" }
-    )
-  )
-}
-
-resource "aws_iam_role_policy_attachment" "tdr_jenkins_read_params_role_attach" {
-  policy_arn = aws_iam_policy.tdr_jenkins_read_params_policy.arn
-  role       = aws_iam_role.tdr_jenkins_read_params_role.name
-}
-
-resource "aws_iam_policy" "tdr_jenkins_read_params_policy" {
-  name   = "TDRJenkinsReadParams${title(var.tdr_environment)}"
-  policy = templatefile("${path.module}/templates/tdr_jenkins_read_params.json.tpl", { environment = var.tdr_environment, account_id = data.aws_caller_identity.current.account_id })
 }
 
 resource "aws_iam_role" "custodian_deploy_role" {
@@ -236,30 +185,6 @@ resource "aws_iam_role_policy_attachment" "grafana_monitoring_policy_attach" {
   role       = aws_iam_role.grafana_monitoring_iam_role.name
 }
 
-resource "aws_iam_role" "jenkins_export_s3_role" {
-  count              = var.tdr_environment == "prod" ? 0 : 1
-  name               = "TDRJenkinsS3ExportRole${title(var.tdr_environment)}"
-  assume_role_policy = templatefile("${path.module}/templates/terraform_assume_role_policy.json.tpl", { account_id = var.tdr_mgmt_account_number })
-  tags = merge(
-    var.common_tags,
-    tomap(
-      { "Name" = "TDR S3 Export Access Role for ECS ${var.tdr_environment}" }
-    )
-  )
-}
-
-resource "aws_iam_policy" "jenkins_export_s3_policy" {
-  count  = var.tdr_environment == "prod" ? 0 : 1
-  name   = "TDRJenkinsS3ExportPolicy${title(var.tdr_environment)}"
-  policy = templatefile("${path.module}/templates/jenkins_export_s3_policy.json.tpl", { environment = var.tdr_environment })
-}
-
-resource "aws_iam_role_policy_attachment" "jenkins_export_s3_attach" {
-  count      = var.tdr_environment == "prod" ? 0 : 1
-  policy_arn = aws_iam_policy.jenkins_export_s3_policy[count.index].arn
-  role       = aws_iam_role.jenkins_export_s3_role[count.index].id
-}
-
 
 resource "aws_iam_role" "github_actions_describe_ec2_role" {
   name               = "TDRGithubActionsDescribeEC2Role${title(var.tdr_environment)}"
@@ -276,11 +201,6 @@ resource "aws_iam_role_policy_attachment" "github_actions_describe_ec2_attach" {
   role       = aws_iam_role.github_actions_describe_ec2_role.id
 }
 
-resource "aws_iam_role" "service_unavailable_deploy_role" {
-  name               = "TDRJenkinsDeployServiceUnavailableRole${title(var.tdr_environment)}"
-  assume_role_policy = templatefile("${path.module}/templates/terraform_assume_role_policy.json.tpl", { account_id = var.tdr_mgmt_account_number })
-}
-
 resource "aws_iam_role" "github_service_unavailable_deploy_role" {
   name               = "TDRGithubActionsDeployServiceUnavailableRole${title(var.tdr_environment)}"
   assume_role_policy = templatefile("./modules/environment-roles/templates/github_assume_role.json.tpl", { account_id = data.aws_caller_identity.current.account_id })
@@ -294,14 +214,4 @@ resource "aws_iam_policy" "github_service_unavailable_deploy_policy" {
 resource "aws_iam_role_policy_attachment" "github_service_unavailable_attach" {
   policy_arn = aws_iam_policy.github_service_unavailable_deploy_policy.arn
   role       = aws_iam_role.github_service_unavailable_deploy_role.id
-}
-
-resource "aws_iam_policy" "service_unavailable_deploy_policy" {
-  name   = "TDRJenkinsDeployServiceUnavailablePolicy${title(var.tdr_environment)}"
-  policy = templatefile("${path.module}/templates/jenkins_service_unavailable_deploy_policy.json.tpl", {})
-}
-
-resource "aws_iam_role_policy_attachment" "service_unavailable_deploy_attach" {
-  policy_arn = aws_iam_policy.service_unavailable_deploy_policy.arn
-  role       = aws_iam_role.service_unavailable_deploy_role.id
 }
