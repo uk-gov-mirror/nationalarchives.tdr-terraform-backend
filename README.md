7f39ca930f9acedada710538f261719b32f472f9
TomJKing <tom.king@nationalarchives.gov.uk>
2023-06-21 09:26:19 +0100

    Upgrade terraform and aws provider versions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TDR Terraform Backend
 
-**Important Note**: tdr-terraform-backend uses v1.3.2 of Terraform. Ensure that Terraform v1.3.2 is installed before proceeding.
+**Important Note**: tdr-terraform-backend uses v1.5.0 of Terraform. Ensure that Terraform v1.5.0 is installed before proceeding.
 
 ## Purpose
 
--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.72.0"
+      version = "5.4.0"
     }
     github = {
       source  = "integrations/github"
       version = "~> 5.12"
     }
   }
-  required_version = ">= 1.3.2"
+  required_version = ">= 1.5.0"
 }
