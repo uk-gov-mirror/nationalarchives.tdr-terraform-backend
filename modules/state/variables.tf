804e9a5a2bfba2c6f41ae1f18d8ed872c8980066
TomJKing <tom.king@nationalarchives.gov.uk>
2025-02-17 07:38:57 +0000

    Encrypt scripts state bucket with CMK

--- modules/state/variables.tf
+++ modules/state/variables.tf
@@ -2,3 +2,7 @@ variable "common_tags" {
   description = "Set of tags that are common for all resources"
   type        = map(string)
 }
+
+variable "terraform_state_bucket_kms_key_arn" {
+  description = "Customer managed encryption key for Terraform state buckets"
+}
