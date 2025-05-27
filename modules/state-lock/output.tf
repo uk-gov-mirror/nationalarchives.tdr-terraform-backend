46e68ce201b4c50928dff5c44783b960e9db129d
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-05-27 13:20:37 +0100

    chore: Dynamo lock table cleanup

--- modules/state-lock/output.tf
+++ modules/state-lock/output.tf
@@ -2,10 +2,6 @@ output "terraform_state_lock_arn" {
   value = aws_dynamodb_table.terraform_state_lock.arn
 }
 
-output "terraform_jenkins_state_lock" {
-  value = aws_dynamodb_table.terraform_state_lock_jenkins.arn
-}
-
 output "terraform_grafana_state_lock_arn" {
   value = aws_dynamodb_table.terraform_state_lock_grafana.arn
 }
