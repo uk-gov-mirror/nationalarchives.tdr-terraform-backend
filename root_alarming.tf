b0b371366986e3b4ef6b8d11a5988d9ce78a580e
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-05-12 11:12:17 +0100

    feat: Allow alarms to be muted without using Cloudwatch

--- root_alarming.tf
+++ root_alarming.tf
@@ -210,14 +210,16 @@ resource "aws_cloudwatch_event_target" "alarms_cloudwatch_all" {
   event_bus_name = aws_cloudwatch_event_bus.alarms_event_bus.name
 }
 
-# Catch any alarm state change to ALARM and send to cloudwatch 
+# Catch any alarm state change to ALARM and send to cloudwatch
+# Skip if alarm name is prefix with "Muted:"
 resource "aws_cloudwatch_event_rule" "alarms_state_change_any_environment_any_alarm" {
   name        = "alarm-state-changes-ALARM-all"
-  description = "Catch all state changes to ALARM and send to slack"
+  description = "Catch all state changes to ALARM and send to slack if the alarm name is not prefixed with 'Muted:'"
   event_pattern = jsonencode({
     source               = ["aws.cloudwatch"],
     detail-type          = ["CloudWatch Alarm State Change"]
     "detail.state.value" = ["ALARM"]
+    "detail.alarmName"   = [{ "anything-but" : { "prefix" : "Muted:" } }]
   })
   event_bus_name = aws_cloudwatch_event_bus.alarms_event_bus.name
 }
