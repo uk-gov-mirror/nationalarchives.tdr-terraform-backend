7da94d5222d05f98655a87e200fb569ee726d2e0
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-06-10 12:12:45 +0100

    fix: Revert root_provider

--- root_provider.tf
+++ root_provider.tf
@@ -1,30 +1,30 @@
 // Because this is a bootstrap stack, local role config is used
 provider "aws" {
   region  = "eu-west-2"
-  profile = "tdr_admin_management"
+  profile = "<Profile configured for account in management>"
 }
 
 provider "aws" {
   alias   = "dev"
   region  = "eu-west-2"
-  profile = "tdr_admin_development"
+  profile = "<Profile configured for account in dev>"
 }
 
 provider "aws" {
   alias   = "intg"
   region  = "eu-west-2"
-  profile = "tdr_admin_integration"
+  profile = "<Profile configured for account in intg>"
 }
 
 provider "aws" {
   alias   = "staging"
   region  = "eu-west-2"
-  profile = "tdr_admin_staging"
+  profile = "<Profile configured for account in staging>"
 
 }
 
 provider "aws" {
   alias   = "prod"
   region  = "eu-west-2"
-  profile = "tdr_admin_production"
+  profile = "<Profile configured for account in prod>"
 }
